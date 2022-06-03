8353cc3a7516bf0c0f3752158a7dfebf7b0b58e2
brandongoode22 <46114167+brandongoode22@users.noreply.github.com>
2022-06-03 09:36:23 -0500

    Added flex property to column width to allow for growing and shrinking window

--- src/Components/Monsters.tsx
+++ src/Components/Monsters.tsx
@@ -1,17 +1,14 @@
-import React, { useState, useMemo, useEffect} from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
-
-let url = 'https://www.dnd5eapi.co/api/monsters';
+let url = "https://www.dnd5eapi.co/api/monsters";
 
 const Monsters = () => {
   const [rowData, setRowData] = useState([]);
 
-  const [columnDefs] = useState([
-    { field: "name", width: 1910 },
-  ]);
+  const [columnDefs] = useState([{ field: "name", flex: 100 }]);
 
   const defaultColDef = useMemo(
     () => ({
@@ -22,16 +19,15 @@ const Monsters = () => {
   );
 
   useEffect(() => {
-   fetch(url)
-  .then(result => result.json())
-   .then(fetchResults => setRowData(fetchResults.results))
-}, [])
-
+    fetch(url)
+      .then((result) => result.json())
+      .then((fetchResults) => setRowData(fetchResults.results));
+  }, []);
 
   return (
     <div
       className="ag-theme-alpine-dark"
-      style={{ height: 1000, width: 1930}}
+      style={{ height: 1000, width: "100vw" }}
     >
       <AgGridReact
         rowData={rowData}
